fbb702900ef9d27ba25978e09ee611e3b71f7c59
SheepTester <ussa.say@gmail.com>
2019-12-25 14:36:06 -0800

    The cloud server, ideally?

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # primitive-cloud-server
 A primitive Node server for Scratch 3.0 cloud variables; it's not made for large-scale projects and can easily be tricked by those pesky JavaScript programmers
+```sh
+npm start
+```
---
+++ index.js
@@ -0,0 +1,120 @@
+const fs = require('fs')
+const path = require('path')
+function readFile (file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+function writeFile (file, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err(err) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+}
+
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 3000 })
+
+const projects = new Map()
+const validProjectId = /^\d+$/
+
+async function getProject (id) {
+  const project = projects.get(id)
+  if (project) return project
+
+  if (!validProjectId.test(id)) return null
+
+  const savePath = path.resolve(__dirname, `./cloud-vars/${id}`)
+  const data = await readFile(savePath)
+  const variables = data ? JSON.parse(data) : {}
+  const connections = new Set()
+  let saveTimeout = null
+  const project = {
+    variables,
+    connections,
+    save () {
+      if (saveTimeout) return
+      saveTimeout = setTimeout(() => {
+        writeFile(savePath, JSON.stringify(variables))
+        saveTimeout = null
+      }, 1000)
+    },
+    announce (announcer, messages) {
+      for (const ws of connections) {
+        if (ws !== announcer) {
+          reply(ws, messages)
+        }
+      }
+    }
+  }
+  projects.set(id, project)
+  return project
+}
+
+function reply (ws, messages) {
+  ws.send(messages.map(message => JSON.stringify(message) + '\n').join(''))
+}
+
+wss.on('connection', ws => {
+  let handshaken = false
+  let project = null
+
+  ws.on('message', data => {
+    const message = JSON.parse(data)
+    switch (message.method) {
+      case 'handshake':
+        if (!handshaken) {
+          handshaken = true
+          getProject(message.project_id)
+            .then(projectData => {
+              if (projectData) {
+                project = projectData
+                project.connections.add(ws)
+                reply(ws, Object.entries(project.variables)
+                  .map(([variable, value]) => ({
+                    method: 'set',
+                    name: variable,
+                    value
+                  })))
+              }
+            })
+        }
+        break
+      case 'create':
+      case 'set':
+        if (project) {
+          project.variables[message.name] = message.value
+          project.announce(ws, [{
+            method: 'set',
+            name: message.name,
+            value: message.value
+          }])
+          project.save()
+        }
+        break
+      case 'rename':
+        if (project) {
+          project.variables[message.new_name] = project.variables[message.name]
+          delete project[message.name]
+          project.announce(ws, [{
+            method: 'set',
+            name: message.new_name,
+            value: message.value
+          }])
+          project.save()
+        }
+        break
+      case 'delete':
+        if (project) {
+          delete project.variables[message.name]
+          project.save()
+        }
+        break
+    }
+  })
+})
